aadd80618c14d5b356272f6b6a3f52d0cb1fd583
Reubotimus <reubencook555@gmail.com>
2024-08-07 15:43:13 +1000

    updated router to show all paths

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const indexRouter = require("./routers/indexRouter");
 const app = express();
 
 app.use('/', indexRouter);
+app.use(express.urlencoded());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("I'm listening!"));
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.send('hello'));
+indexRouter.get('/', (req, res) => res.send('all items'));
+
+indexRouter.get('/categories', (req, res) => res.send('categories'));
+indexRouter.post('/categories', (req, res) => res.send('new category'));
+indexRouter.put('/categories', (req, res) => res.send('update category'));
+indexRouter.delete('/categories', (req, res) => res.send('delete category'));
+
+indexRouter.get('/categories/:category', (req, res) => res.send(req.params.category));
+indexRouter.post('/categories/:category', (req, res) => res.send('new item with category ' + req.params.category));
+indexRouter.put('/categories/:category', (req, res) => res.send('update item with category ' + req.params.category));
+indexRouter.delete('/categories/:category', (req, res) => res.send('delete item with category ' + req.params.category));
 
 module.exports = indexRouter;
